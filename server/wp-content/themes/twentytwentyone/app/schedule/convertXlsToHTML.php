b87db59bfcbd6e09fe6b8da57842a83484a8205e
zendau <cowalyow2012@gmail.com>
2023-11-06 15:02:13 +0300

    refactor(server/schedule):  Improve file structure and optimization

--- server/wp-content/themes/twentytwentyone/app/schedule/convertXlsToHTML.php
+++ server/wp-content/themes/twentytwentyone/app/schedule/convertXlsToHTML.php
@@ -1,7 +1,7 @@
 <?php
 
 
-require __DIR__ . '/../../../../vendor/autoload.php';
+require __DIR__ . '/../../../../../vendor/autoload.php';
 
 use PhpOffice\PhpSpreadsheet\IOFactory;
 
--- server/wp-content/themes/twentytwentyone/assets/js/schedule-form.js
+++ server/wp-content/themes/twentytwentyone/assets/js/schedule-form.js
@@ -1,4 +1,17 @@
-document.addEventListener('DOMContentLoaded', function () {
-  // Ваш JavaScript-код здесь
-  console.log('Привет, мир!');
-});
+const form = document.querySelector('form')
+const alert = document.querySelector('.alert')
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault()
+
+  const formData = new FormData(form)
+
+  const res = await fetch(wpAjax.ajaxurl, {
+    method: 'POST',
+    body: formData
+  })
+
+  const statusMessage = await res.text()
+
+  alert.innerHTML = statusMessage
+})
