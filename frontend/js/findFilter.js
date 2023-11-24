0c2b3a82fe3d147078c2c70f4af85242963b3934
OKIS <67818930+okis-okis@users.noreply.github.com>
2023-11-24 20:37:35 +0300

    Fixed posts pages
    Fixed posts (blogs) list and post page

--- frontend/js/findFilter.js
+++ frontend/js/findFilter.js
@@ -2,6 +2,40 @@ const findButton = document.getElementsByClassName(
   "posts__container-posts-find-order-button"
 );
 
+const filterButton = document.getElementsByClassName(
+  "posts__container__mobile_filter__button"
+);
+
+const filterBackground = document.getElementsByClassName(
+  "posts__container__mobile_filter"
+);
+
+const postsFind = document.getElementsByClassName(
+  "posts__container-posts-find"
+);
+
+const postsFilter = document.getElementsByClassName(
+  "posts__container-posts-filter"
+);
+
+filterButton[0].onclick = () => {
+  if (
+    !filterBackground[0].classList.contains(
+      "posts__container__mobile_filter-active"
+    )
+  ) {
+    filterBackground[0].classList.add("posts__container__mobile_filter-active");
+    postsFind[0].classList.add("posts__container-posts-find-active");
+    postsFilter[0].classList.add("posts__container-posts-filter-active");
+  } else {
+    filterBackground[0].classList.remove(
+      "posts__container__mobile_filter-active"
+    );
+    postsFind[0].classList.remove("posts__container-posts-find-active");
+    postsFilter[0].classList.remove("posts__container-posts-filter-active");
+  }
+};
+
 for (let i = 0; i < findButton.length; i++) {
   findButton[i].onclick = () => {
     defaultState();
