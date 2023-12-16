b23dca29b15cfeb0b656c166da7653726912ed4b
Zendau <cowalyow2012@gmail.com>
2023-12-16 16:48:24 +0300

    feat(server): Add error page

--- server/wp-content/themes/scs/404.php
+++ server/wp-content/themes/scs/404.php
@@ -1,1 +1,9 @@
-<?php ?>
+<?php get_header(); ?>
+
+<div class="error__container">
+  <p class="error__container-message">Ой, что-то пошло не так!</p>
+  <p class="error__container-code">Ошибка 404</p>
+  <a href="/" class="default-btn error-btn">Вернуться на главную страницу</a>
+</div>
+
+<?php get_footer(); ?>
